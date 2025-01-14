08ce6a03b2b79b6e80064ebac82682209738e52a
Sam Kamau <skmbugua5@gmail.com>
2025-01-14 16:01:56 +0300

    Insert New Records Functionalty Added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -61,4 +61,29 @@ def customer_record(request, pk):
 		return render(request, 'record.html', {'customer_record': customer_record})
 	else:
 		messages.success(request, "You must be logged in to view that page")
+		return redirect('home')
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		# Delete record
+		customer_record = Record.objects.get(id=pk)
+		customer_record.delete()
+		messages.success(request, "Record deleted Successfully")
+		return redirect('home')
+	else:
+		messages.success(request, "You must be logged in to delete a record")
+		return redirect('home')
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added Successfully!")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form': form})
+
+	else:
+		messages.success(request, "You must be logged in to add a record")
 		return redirect('home')
